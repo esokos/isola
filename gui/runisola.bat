bf011b58910db4e1fc31d4e916c6882ada4d6e75
esokos <esokos@upatras.gr>
2014-02-27 11:37:03 +0000

    27_02_2014

---
+++ gui/runisola.bat
@@ -0,0 +1,12 @@
+Rem Running inversion
+isola12c
+rem isola12a.exe
+rem isola09.exe
+
+
+Rem Running norm
+norm12c.exe
+rem norm
+
+Rem Running time function
+rem timfun.exe
---
+++ gui/runisola_auto.bat
@@ -0,0 +1,8 @@
+Rem Running inversion
+isola11_autom.exe
+
+Rem Running norm
+norm11b.exe
+
+Rem Running time function
+rem timfun.exe
